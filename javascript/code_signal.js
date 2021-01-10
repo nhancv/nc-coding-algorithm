f3f89002e9c9be754fef075ad42e23d835ebdb45
Nhan Cao <nhancv92@gmail.com>
2021-01-10 08:53:57 +0700

    feat: update js example

--- javascript/code_signal.js
+++ javascript/code_signal.js
@@ -1,5 +1,16 @@
 function isIncreasingDigitsSequence(n) {
-    return n;
+    const str = `${n}`;
+
+    for (let i = 0; i < str.length - 1; i++) {
+        for (let j = i + 1; j < str.length; j++) {
+            const a = parseInt(str[i]);
+            const b = parseInt(str[j]);
+            if(a >= b) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 const res = isIncreasingDigitsSequence(12345);
